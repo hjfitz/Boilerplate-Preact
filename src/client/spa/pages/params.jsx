e6fbe382467b515ae763296c10599ac61ecc3fdf
Harry <hfitz83@gmail.com>
2018-07-14 09:23:59 +0100

    reset

--- src/client/spa/pages/params.jsx
+++ src/client/spa/pages/params.jsx
@@ -1,16 +1,26 @@
 import { h, Component } from 'preact';
 
+/**
+ * Basic component.
+ * It follows the react component lifecycle!
+ */
 export default class Params extends Component {
   constructor(props) {
     super(props);
     console.log(props);
   }
 
-  componentDidMount() {
-    console.log('oi');
-  }
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
 
   render() {
     return <h1>{this.props.matches.param}</h1>;
   }
+}
+
+Params.propTypes = {
+ matches: PropTypes.object, 
 }
